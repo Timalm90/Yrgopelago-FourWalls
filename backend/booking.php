9f0be4d98c7f317efb1393ce740cb21b4a929f5d
Timalm90 <timalm90@gmail.com>
2025-12-17 11:55:21 +0100

    Got connection to database \o/

--- backend/booking.php
+++ backend/booking.php
@@ -1,12 +1,41 @@
 <?php
 
-if (isset($_POST['guestId'], $_POST['transferCode'], $_POST['arrival'], $_POST['departure'], $_POST['room'])) {
 
-    $guestId = $_POST['guestId'];
-    $transferCode = $_POST['transferCode'];
-    $arrival = $_POST['arrival'];
-    $departure = $_POST['departure'];
-    $room = $_POST['room'];
+
+if (isset(
+    $_POST['guestId'],
+    $_POST['transferCode'],
+    $_POST['arrival'],
+    $_POST['departure'],
+    $_POST['room'],
+)) {
+
+    $guestId = htmlspecialchars($_POST['guestId'] ?? '');
+    $transferCode = htmlspecialchars($_POST['transferCode'] ?? '');
+    $arrival = ($_POST['arrival'] ?? '');
+    $departure = ($_POST['departure'] ?? '');
+    $room = $_POST['room'] ?? '';
+
+    $featuresUsed = $_POST['features'] ?? [];
+
+
+
+    $database = new PDO('sqlite:database/database.db');
+    $statement = $database->query('SELECT * FROM rooms');
+    $rooms = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+    var_dump($rooms);
+
+    // foreach ($rooms as $room) {
+
+    //     echo $room['tier'] . "<br>";
+    // }
 };
 
-var_dump($guestId);
+
+
+
+
+
+// var_dump($_POST['features']);
